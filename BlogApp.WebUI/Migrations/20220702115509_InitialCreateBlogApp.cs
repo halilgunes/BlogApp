1ee940428c714249bd144845071dd184422a4799
halilgunes <43631894+halilgunes@users.noreply.github.com>
2022-07-02 15:11:05 +0300

    İlk commit

---
+++ BlogApp.WebUI/Migrations/20220702115509_InitialCreateBlogApp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BlogApp.WebUI.Migrations
+{
+    public partial class InitialCreateBlogApp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
